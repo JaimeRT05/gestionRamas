cc964925406d18695fb28e1d2d93001dddcb5f33
JaimeRT05 <romantejerojaime@gmail.com>
2024-10-16 08:46:28 +0200

    Testeando ramas 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ package GITIGNORE;
 
 public class HelloWorld {
     public static void main (String [] args) {
+        
+        String rojo = "\033[32m";
+        String azul = "\033[34m";
         System.out.println("Hola Mundo! from GITT");
         System.out.println("Aquí estamos haciendo pruebas en Git");
     }
