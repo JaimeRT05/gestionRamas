20b3a20bd42e5ef668b96d88888b50e84cd1dbd3
JaimeRT05 <romantejerojaime@gmail.com>
2024-10-16 09:30:18 +0200

    Error de color arreglado

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package GITIGNORE;
 public class HelloWorld {
     public static void main (String [] args) {
         
-        String rojo = "\033[32m";
+        String rojo = "\033[31m";
         String azul = "\033[34m";
         System.out.println(rojo+ "Hola Mundo! from GITT");
         System.out.println(azul +"Aquí estamos haciendo pruebas en Git");
